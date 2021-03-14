d64a07a3bd829ee1cf528dee2b41867ba3b98cc8
Dustin <maurer.dustin@gmail.com>
2021-03-14 16:50:09 -0700

    make init look like neural_ode

--- options_strategy_backtester/__init__.py
+++ options_strategy_backtester/__init__.py
@@ -1,7 +1,6 @@
 # Python program to backtest options strategies
 
-class Option():
-    def __init__(self, kind):
-        if kind.lower() not in ["put","call"]:
-            raise TypeError("The option must be either a put or a call.")
-        self.kind = 1 if kind.lower() == "put" else -1
+import math
+import numpy as np
+
+from . import option
